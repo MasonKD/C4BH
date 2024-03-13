ee8f11b9ff33eef16ee3c04728f4872be3d11aeb
Noah George <98036063+NoahWGeorge@users.noreply.github.com>
2024-03-13 14:14:55 -0400

    style changes and work on mirth API

--- client/src/Mirth.js
+++ client/src/Mirth.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Mirth.css';
 import logoImage from './images/C4BHLogo.png';
+import mirthImage from './images/Mirth.png';
+
 
 const Logo = () => (
   <div className="logo">
@@ -10,25 +12,56 @@ const Logo = () => (
 );
 
 const Mirth = () => {
-  const navigate = useNavigate(); // Initialize navigate function
+  const [logs, setLogs] = useState([]);
+  const navigate = useNavigate();
 
-  // Handler for the Sign Out button
+  useEffect(() => {
+  const apiEndpoint = 'https://18.205.165.171:8443/api/channels/4cc843b2-84e0-4ee0-805a-65052a37d0d/messages';
+  const queryParams = '?status=RECEIVED&limit=20';
+  const headers = {
+    'Accept': 'application/json',
+    'X-Requested-With': 'OpenAPI' // This header is usually used to identify Ajax requests. Make sure this is needed by your API.
+    // If you have a bearer token, you would also include it here:
+    // 'Authorization': `Bearer your_access_token`
+  };
+
+  fetch(apiEndpoint + queryParams, {
+    method: 'GET', // This is the default method, but it's here for clarity.
+    headers: headers
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP status ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => setLogs(data.messages)) // Assuming the data structure is similar to that in your screenshot.
+  .catch(error => console.error('Error fetching data:', error));
+}, []);
+
+
   const handleSignOut = () => {
     navigate('/'); // Redirects to the landing page
   };
-
-
 
   return (
     <div className="main-container">
       <header className="header">
         <Logo />
-        <div>
-          <div className="user-participant">User Participant: Provider A</div>
-          <button className="signout-button" onClick={handleSignOut}>Sign Out</button> {/* Sign Out Button */}
+        <div className="user-participant">
+          User Participant: Provider A
+          <button className="signout-button" onClick={handleSignOut}>
+            Sign Out
+          </button>
         </div>
       </header>
-      
+      <h1>Mirth Connect Logs</h1>
+      <img src={mirthImage} alt="Mirth" className="mirth-image" />
+      <ul>
+        {logs.map(log => (
+          <li key={log.id}>Message ID: {log.id}, Content: {log.content}</li>
+        ))}
+      </ul>
     </div>
   );
 };
