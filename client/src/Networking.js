f088f50c60add5120c6b96765fa12029cd3aa895
NotAnotherDoorNob <38090078+NotAnotherDoorNob@users.noreply.github.com>
2024-03-21 14:33:44 -0400

    add images

--- client/src/Networking.js
+++ client/src/Networking.js
@@ -25,6 +25,7 @@ const Networking = () => {
   const [selectedCities, setSelectedCities] = useState([]);
   const [selectedTypes, setSelectedTypes] = useState([]);
 
+
   const cityOptions = useMemo(() => {
     const uniqueCities = [...new Set(data.map(item => item.City))];
     return uniqueCities
@@ -40,6 +41,7 @@ const Networking = () => {
   }, []);
 
 
+
   const handleCityChange = selectedOptions => {
     setSelectedCities(selectedOptions || []);
   };
@@ -48,18 +50,13 @@ const Networking = () => {
     setSelectedTypes(selectedOptions || []);
   };
 
+
   const uniqueParticipants = useMemo(() => {
     let participants = [];
     data.forEach(item => {
       if (!participants.some(participant => participant.Participant_Name === item.Participant_Name)) {
         participants.push({
           Participant_Name: item.Participant_Name,
-          Type: item.Type !== 'NULL' ? item.Type : "",
-          ID: item.Participant_ID !== 'NULL' ? item.Participant_ID : "",
-          Request_for_Information: item.Request_for_Information !== 'NULL' ? item.Request_for_Information : "",
-          Information_Delivery: item.Information_Delivery !== 'NULL' ? item.Information_Delivery : "",
-          Requests_for_Notification_of_ADT_Events: item.Requests_for_Notification_of_ADT_Events !== 'NULL' ? item.Requests_for_Notification_of_ADT_Events : "",
-          City: item.City !== 'NULL' ? item.City : ""
           Type: item.Type !== 'NULL' ? item.Type : "",
           ID: item.Participant_ID !== 'NULL' ? item.Participant_ID : "",
           Request_for_Information: item.Request_for_Information !== 'NULL' ? item.Request_for_Information : "",
@@ -74,10 +71,10 @@ const Networking = () => {
 
   const renderMarkers = () => {
     return data
-      .filter(item => 
-        (selectedCities.length === 0 || selectedCities.some(city => city.value === item.City)) &&
-        (selectedTypes.length === 0 || selectedTypes.some(type => type.value === item.Type))
-      )
+    .filter(item => 
+      (selectedCities.length === 0 || selectedCities.some(city => city.value === item.City)) &&
+      (selectedTypes.length === 0 || selectedTypes.some(type => type.value === item.Type))
+    )
       .map((item, index) => {
         const parts = item.GeoPoint.split(', ');
         if (parts.length === 2) {
@@ -125,67 +122,108 @@ const Networking = () => {
           </button>
         </div>
       </header>
-      <div className="shared-title-container">
-        <h2 className="shared-title">Participant Map</h2>
-      </div>
-      <div className="content-container">
-        <div className="city-selector">
-          <Select
-            isMulti
-            options={cityOptions}
-            className="city-select"
-            placeholder="Select Cities"
-            onChange={handleCityChange}
-            value={selectedCities}
-          />
-          <Select
-            isMulti
-            options={typeOptions}
-            className="type-select"
-            placeholder="Select Types"
-            onChange={handleTypeChange}
-            value={selectedTypes}
-            />
+      <div className='main'>
+        <div className='section'>
+          <div className='container' id='one-one'>
+            <div className="shared-title-container">
+                <h2 className="shared-title">Participants</h2>
+            </div>
+            <div className='flex-col'>
+            <div id='test'>
+                  <img src={DSAImage} alt="Table of Sandbox Intermediaries" />
+              </div>
+              <div id='test'>
+                  <img src={interImage} alt="Table of Sandbox Intermediaries" />
+              </div>
+
+            </div>
+
+              
+          </div>
+          <div className='container' id='one-two'>
+            <div className="shared-title-container">
+              <h2 className="shared-title">Geographic View</h2>
+            </div>
+            <div className="content-container">
+              <div className="city-selector">
+                <Select
+                  isMulti
+                  options={cityOptions}
+                  className="city-select"
+                  placeholder="Select Cities"
+                  onChange={handleCityChange}
+                  value={selectedCities}
+                />
+                <Select
+                  isMulti
+                  options={typeOptions}
+                  className="type-select"
+                  placeholder="Select Types"
+                  onChange={handleTypeChange}
+                  value={selectedTypes}
+               />
+              </div>
+              <MapContainer center={[37.505915, -120.505943]} zoom={6} scrollWheelZoom={false} className="leaflet-container">
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {renderMarkers()}
+              </MapContainer>
+            </div>
+          </div>
+
         </div>
-        <MapContainer center={[39.572289, -98.036979]} zoom={4} scrollWheelZoom={false} className="leaflet-container">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {renderMarkers()}
-        </MapContainer>
-      </div>
-      <div className="shared-title-container">
-        <h2 className="shared-title">Participant Table</h2>
-      </div>
-      <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Participant</th>
-              <th>Type</th>
-              <th>ID</th>
-              <th>Request for Information</th>
-              <th>Information Delivery</th>
-              <th>Requests for Notification of ADT Events</th>
-              <th>City</th>
-            </tr>
-          </thead>
-          <tbody>
-            {uniqueParticipants.map((participant, index) => (
-              <tr key={index}>
-                <td>{participant.Participant_Name}</td>
-                <td>{participant.Type}</td>
-                <td>{participant.ID}</td>
-                <td>{participant.Request_for_Information}</td>
-                <td>{participant.Information_Delivery}</td>
-                <td>{participant.Requests_for_Notification_of_ADT_Events}</td>
-                <td>{participant.City}</td>
+
+        <div className='section'>
+          <div className='container' id='one'>
+          <div className="shared-title-container">
+              <h2 className="shared-title">Network View</h2>
+            </div>
+            <div className="content-container">
+              <SankeyChart />
+          </div>
+        </div>
+        </div>
+
+        <div className='section' id='hide-me'>
+
+          <div className="shared-title-container">
+            <h2 className="shared-title">Participant Table</h2>
+          </div>
+          <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Participant</th>
+                <th>Type</th>
+                <th>ID</th>
+                <th>Request for Information</th>
+                <th>Information Delivery</th>
+                <th>Requests for Notification of ADT Events</th>
+                <th>City</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {uniqueParticipants.map((participant, index) => (
+                <tr key={index}>
+                  <td>{participant.Participant_Name}</td>
+                  <td>{participant.Type}</td>
+                  <td>{participant.ID}</td>
+                  <td>{participant.Request_for_Information}</td>
+                  <td>{participant.Information_Delivery}</td>
+                  <td>{participant.Requests_for_Notification_of_ADT_Events}</td>
+                  <td>{participant.City}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          </div>
+        </div>
+
+
       </div>
+
     </div>
   );
 };
