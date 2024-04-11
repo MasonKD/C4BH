9157bb7033d804bf0585e7a89aa11b55e9f9c756
Noah George <98036063+NoahWGeorge@users.noreply.github.com>
2024-04-11 16:16:06 -0400

    adding skeleton SMILE page

---
+++ client/src/SmileCDR.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './useCase.css';
+import logoImage from './images/C4BHLogo.png';
+
+
+
+const Logo = () => (
+  <div className="logo">
+    <img src={logoImage} alt="Connecting for Better Health" />
+  </div>
+);
+
+const useCase = () => {
+  
+  const navigate = useNavigate();
+
+  
+  
+
+  const handleSignOut = () => {
+    navigate('/');
+  };
+
+  
+
+  return (
+    <div className='main-container'>
+      <header className="header">
+        <Logo />
+        <div className="user-participant">
+          User: C4BH Admin
+          <button className="signout-button" onClick={handleSignOut}>
+            Sign Out
+          </button>
+          <button className="back-button" onClick={() => navigate(-1)}>Back</button>
+        </div>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+};
+
+export default useCase;
