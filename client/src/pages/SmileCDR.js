4f2ce43ec0576337fa719bece8b649b512792fba
Noah George <98036063+NoahWGeorge@users.noreply.github.com>
2024-04-26 08:41:59 -0400

    switch test URLs back to dev URLs

--- client/src/pages/SmileCDR.js
+++ client/src/pages/SmileCDR.js
@@ -90,7 +90,7 @@ const SmileCDR = () => {
       setError('');
 
       try {
-        const response = await fetch('http://127.0.0.1:3001/smile-query');
+        const response = await fetch('https://sbx.connectingforbetterhealth.com/api/smile-query');
         if (!response.ok) {
           throw new Error(`HTTP status ${response.status}`);
         }
